4a97ddba042540a77abc99c0f0c23201c9df7ebe
Mike Tagg <mdtagg91@gmail.com>
2022-10-03 12:36:41 -0700

    Event listener added to operation buttons to transition button color once clicked

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const buttons = document.querySelectorAll('.button')
-buttons.forEach(button => button.addEventListener('click', changeDisplay))
+const numbers = document.querySelectorAll('.dark-gray')
+numbers.forEach(button => button.addEventListener('click', changeDisplay))
 
 function changeDisplay(e) {
     console.log(e)
     let result = document.querySelector('.result')
     result.textContent = e.target.textContent
 }
+
+
+const operators = document.querySelectorAll('.orange')
+operators.forEach(button => button.addEventListener('click', changeColor))
+
+function changeColor(e) {
+    
+    operators.forEach(button => {
+        const classLists = Array.from(button.classList)
+        if(classLists.includes('transition')) {
+            button.classList.remove('transition')
+        }
+        e.target.classList.add('transition');
+})}
